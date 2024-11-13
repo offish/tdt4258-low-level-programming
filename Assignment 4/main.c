45a522a022430d8ae8ea8af6870d834e83a39b86
offish <overutilization@gmail.com>
2024-11-13 14:34:51 +0100

    busy wait

--- Assignment 4/main.c
+++ Assignment 4/main.c
@@ -1,4 +1,4 @@
-#include "usart.h"
+#include "usart.c"
 
 void AC_init()
 {
@@ -8,6 +8,12 @@ void AC_init()
     // Disable digital input buffer and pull-up resistor for pin PD2
     PORTD.PIN2CTRL = PORT_ISC_INPUT_DISABLE_gc;
 
+    PORTA.PINCTRLUPD = 0xFF; // Update all pins on PORT A
+    PORTB.PINCTRLUPD = 0xFF; // Update all pins on PORT B
+    PORTC.PINCTRLUPD = 0xFF; // Update all pins on PORT C
+    // PORTD.PINCTRLUPD = 0xFF;  // Update all pins on PORT D
+    PORTE.PINCTRLUPD = 0xFF; // Update all pins on PORT E
+
     // Step 2
     // Select the positive and negative input sources
     AC0.MUXCTRL = AC_MUXPOS_AINP0_gc | AC_MUXNEG_DACREF_gc;
@@ -15,20 +21,20 @@ void AC_init()
     // Step 3 & 4
     // Enable output and the Analog Comparator
     AC0.CTRLA = AC_OUTEN_bm | AC_ENABLE_bm;
+
+    // Set the DAC reference
+    // DACREF = (0.1 / 1.024) * 256 = 25
+    AC0.DACREF = 25;
 }
 
 void VREF_init(void)
 {
     VREF.ACREF = VREF_REFSEL_1V024_gc;
-    VREF.DAC0REF = 25 / 256 * VREF_REFSEL_1V024_gc;
-
-    // AC0.DACREF = 0x18;
 }
 
 void LED_init()
 {
     PORTA.DIRSET = PIN2_bm;
-    // PORTA.PIN2CTRL = PORT_PINCTRLSET_7_bm;
 }
 
 void set_LED_on()
@@ -53,17 +59,20 @@ int main()
     AC_init();
     VREF_init();
     LED_init();
+    USART3_Init();
 
     while (1)
     {
         // Implement the busy-waiting scheme
         if (AC_above_threshold())
         {
-            set_LED_on();
+            // USART3_SendString("AC above threshold\n");
+            set_LED_off();
         }
         else
         {
-            set_LED_off();
+            // USART3_SendString("AC below threshold\n");
+            set_LED_on();
         }
     }
     return 0;
